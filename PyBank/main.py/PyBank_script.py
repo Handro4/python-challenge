0c03439d68f5721ae9c966a889d3e935499f9ece
Edgar Alvarado <eaalvaradojr@gmail.com>
2023-11-13 17:48:58 -0700

    Attempt 3

--- PyBank/main.py/PyBank_script.py
+++ PyBank/main.py/PyBank_script.py
@@ -1,30 +1,32 @@
+#1 Import
+import os
 import csv
 
-# Define the file path
-file_path = r"C:\Users\eaalv\Bootcamp\python-challenge\PyBank\Resources\budget_data.csv"
+#2 FilePath
+csvpath = r"C:\Users\eaalv\Bootcamp\python-challenge\PyBank\Resources\budget_data.csv"
 
-# Initialize variables
+#3 Variables
 total_months = 0
 net_total = 0
 previous_profit_loss = 0
 profit_loss_changes = []
 dates = []
 
-# Read the CSV file
-with open(file_path, newline='') as csvfile:
+#4 Read in the CSV file
+with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     
     # Read the header row
     header = next(csvreader)
     
     for row in csvreader:
-        # Count total months
+        # Calculate total months
         total_months += 1
         
-        # Calculate net total profit/loss
+        # Calculate net total amount of "Profit/Losses"
         net_total += int(row[1])
         
-        # Calculate change in profit/loss and store in list
+        # Calculate changes in "Profit/Losses" over time and store in list for later average calculation
         change = int(row[1]) - previous_profit_loss
         profit_loss_changes.append(change)
         
@@ -34,16 +36,16 @@ with open(file_path, newline='') as csvfile:
         # Update previous profit/loss for the next iteration
         previous_profit_loss = int(row[1])
 
-# Calculate average change
+#5 Calculate average change
 average_change = round(sum(profit_loss_changes[1:]) / (total_months - 1), 2)
 
-# Find the greatest increase and decrease in profits
+#6 Find the greatest increase and decrease in profits
 greatest_increase = max(profit_loss_changes)
 greatest_increase_date = dates[profit_loss_changes.index(greatest_increase)]
 greatest_decrease = min(profit_loss_changes)
 greatest_decrease_date = dates[profit_loss_changes.index(greatest_decrease)]
 
-# Print results to the terminal
+#7 Print results to the terminal
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {total_months}")
@@ -52,7 +54,7 @@ print(f"Average Change: ${average_change}")
 print(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})")
 print(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})")
 
-# Export results to a text file
+#8 Export results to a text file in "analysis" folder in PyBank 
 output_file_path = r"C:\Users\eaalv\Bootcamp\python-challenge\PyBank\analysis\financial_analysis.txt"
 with open(output_file_path, 'w') as output_file:
     output_file.write("Financial Analysis\n")
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # python-challenge
+PyBank Script
+#1 Import
+Standard step we did in python homework
+
+#2 Filepath
+I had to watch a youtube video which showed me how to copy the filepath for the budget_data csv file directly from VScode https://www.youtube.com/watch?v=b678IZ1O1pM
+I also asked chatGPT why I received a SyntaxError: (unicode error) and it recommended using the prefix 'r' to use a raw string literal, when I attempted to do os.path.join it kept giving me errors while this way worked and the code ran. Google says its a windows issue??
+
+#3 Variables 
+This was covered in 3.1 activites, specifically 03-Ins_Variables
+
+#4 Read in the CSV File
+this was covered in 3.2 activites, specifically 09-Stu_ReadComicBooksCSV, 08-Ins_ReadCSV
+
+#5 Calculate average change
+I found various examples of this in stack overflow which said to round the sum(average) to 2 decible places round(sum((x/x), 2) 
+
+#6 Find the greatest increase and decrease in profits
+
+
+#7 Print results to the terminal
+We've learned to print in various homework assignments and 3.1 03-Ins_Variables activity was useful 
+
+#8 Export results to a text file in "analysis" folder in PyBank
+This was covered in 3.2 activites 10-Ins_WriteCSV homework
