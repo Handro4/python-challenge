63f59aa7eb0ffa7e743531b8af6f129a8cac469e
Edgar Alvarado <eaalvaradojr@gmail.com>
2023-11-13 17:52:47 -0700

    Attempt 4

--- PyPoll/main.py/PyPoll_script.py
+++ PyPoll/main.py/PyPoll_script.py
@@ -1,15 +1,16 @@
+#1 Import
 import csv
 
-# Filepath
+#2 Filepath
 file_path = r"C:\Users\eaalv\Bootcamp\python-challenge\PyPoll\Resources\election_data.csv"
 
-# Variables
+#3 Variables
 total_votes = 0
 candidates = {}
 winner = ""
 winner_votes = 0
 
-# Read in the CSV file
+#4 Read in the CSV file
 with open(file_path, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     
@@ -17,28 +18,28 @@ with open(file_path, newline='') as csvfile:
     header = next(csvreader)
     
     for row in csvreader:
-        # Count total votes
+        # Calculate total number of votes cast
         total_votes += 1
         
-        # Track candidate votes
+        # Calculate list of candidates who received votes
         candidate = row[2]
         if candidate not in candidates:
             candidates[candidate] = 1
         else:
             candidates[candidate] += 1
 
-# Print results to the terminal
+#5 Print results to the terminal
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {total_votes}")
 print("-------------------------")
 
-# Calculate and print each candidate's total votes and percentage
+#6 Calculate each candidate's percentage of votes and total number of votes won
 for candidate, votes in candidates.items():
     percentage = round((votes / total_votes) * 100, 3)
     print(f"{candidate}: {percentage}% ({votes})")
     
-    # Determine the winner
+    # Calculate the winner based on popular vote
     if votes > winner_votes:
         winner = candidate
         winner_votes = votes
@@ -47,7 +48,7 @@ print("-------------------------")
 print(f"Winner: {winner}")
 print("-------------------------")
 
-# Export results to a text file
+#7 Export results to a text file in 'analysis' folder in PyPoll
 output_file_path = r"C:\Users\eaalv\Bootcamp\python-challenge\PyPoll\analysis\election_results.txt"
 with open(output_file_path, 'w') as output_file:
     output_file.write("Election Results\n")
